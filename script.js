d93d112d8f47119e388d3c39a78cd45a00b6c60b
Jared Rairdon <jaredsrairdon@gmail.com>
2023-08-21 21:25:07 -0500

    Added comments for clarity

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Gameboard module
 const Gameboard = (() => {
+    // Declaring board array and filling each index with blank values.
+    // This array will be used to store the moves played each turn
     let board = Array(9).fill('');
 
     const getBoard = () => board; // returns board array when called
+
+    
+    /*
+     * The function `updateCell` updates a cell in a board array with a given marker if the cell is empty,
+     * and returns true if the move is valid, otherwise it returns false.
+     * @param index - The index parameter represents the position of the cell in the board array that needs
+     * to be updated.
+     * @param marker - The `marker` parameter represents the symbol or marker that will be placed in the
+     * cell of the game board. It can be any value that represents a player's move, such as 'X' or 'O'.
+     * @returns The function `updateCell` returns `true` if the move is valid and `false` if the move is
+     * invalid.
+     */
 
     const updateCell = (index, marker) => {
         if (board[index] === '') {
@@ -22,35 +36,49 @@ const Player = (name, markerIcon) => {
     return {name, markerIcon};
 };
 
+// Game Module
 const Game = (() => {
     let currentPlayer;
-    let currentPlayerLabel = document.getElementById('current-player');
+    let currentPlayerElement = document.getElementById('current-player');
     let gameOver = false;
 
     const player1 = Player('Player 1', 'X');
     const player2 = Player('Player 2', 'O');
 
+    // Arrow function to toggle the currentPlayer, then adjusts the text in the currentPlayerElement
     const togglePlayer = () => {
         currentPlayer = (currentPlayer === player1) ? player2 : player1;
-        currentPlayerLabel.innerText = currentPlayer.name;
+        currentPlayerElement.innerText = currentPlayer.name;
     };
 
+    /*
+        Arrow function to play a turn of the game.
+        It calls the updateCell method from the Gameboard module to check if it is a valid move.
+        If the move is valid, updateCell
+
+        @param index - The index parameter represents the position of the cell in the board array
+        that the player clicked on
+    */
     const playTurn = (index) => {
         if (!gameOver) {
-            const validMove = Gameboard.updateCell(index, currentPlayer.markerIcon);
+            const validMove = Gameboard.updateCell(index, currentPlayer.markerIcon); // Gameboard.updateCell will update the cell content, then return boolean whether the move was valid or not.
             console.log(`playTurn => validMove: ${validMove}`)
-            if (validMove) {
-                // check for win or tie conditions
-                UI.renderGameboard();
-                togglePlayer();
-
+            if (validMove) { // If the move is valid
+                // TODO: check for win or tie conditions
+                UI.renderGameboard(); // Re-renders the gameboard to display the the new moves
+                togglePlayer(); // Toggles the current player
             }
         }
     };
 
+
+    /*
+    * The startGame arrow function initializes the game by setting the current player, rendering the gameboard,
+    * and attaching cell listeners.
+    */
     const startGame = () => {
         currentPlayer = player1;
-        currentPlayerLabel.innerText = currentPlayer.name;
+        currentPlayerElement.innerText = currentPlayer.name;
         gameOver = false;
         UI.renderGameboard();
         UI.attachCellListeners();
@@ -61,6 +89,10 @@ const Game = (() => {
 })();
 
 const UI = (() => {
+    /**
+     * The `renderGameboard` function updates the display of the game board by setting the content of each
+     * cell based on the value stored in the corresponding index of the board array.
+     */
     const renderGameboard = () => {
         const board = Gameboard.getBoard();
         const cells = document.querySelectorAll('.cell');
@@ -74,9 +106,13 @@ const UI = (() => {
         console.log(`renderGameboard => Gameboard re-rendered!`);
     };
 
+    /**
+     * The function `attachCellListeners` attaches click event listeners to all cells with the class "cell"
+     * and calls the `Game.playTurn` function with the index of the clicked cell as an argument, but only
+     * if the game is not over.
+     */
     const attachCellListeners = () => {
         const cells = document.querySelectorAll('.cell');
-    
         cells.forEach((cell, index) => {
           cell.addEventListener('click', () => {
             if (!Game.gameOver) {
