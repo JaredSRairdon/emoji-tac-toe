ef0022186b07a252208e8a99854a47e53d727c44
Jared Rairdon <jaredsrairdon@gmail.com>
2023-08-23 23:06:50 -0500

    added pregame menu to select player names and an emoji

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const Gameboard = (() => {
 
     const getBoard = () => board; // returns board array when called
 
-    
     /*
      * The function `updateCell` updates a cell in a board array with a given marker if the cell is empty,
      * and returns true if the move is valid, otherwise it returns false.
@@ -36,58 +35,7 @@ const Player = (name, markerIcon) => {
     return {name, markerIcon};
 };
 
-// Game Module
-const Game = (() => {
-    let currentPlayer;
-    let currentPlayerElement = document.getElementById('current-player');
-    let gameOver = false;
-
-    const player1 = Player('Player 1', 'X');
-    const player2 = Player('Player 2', 'O');
-
-    // Arrow function to toggle the currentPlayer, then adjusts the text in the currentPlayerElement
-    const togglePlayer = () => {
-        currentPlayer = (currentPlayer === player1) ? player2 : player1;
-        currentPlayerElement.innerText = currentPlayer.name;
-    };
-
-    /*
-        Arrow function to play a turn of the game.
-        It calls the updateCell method from the Gameboard module to check if it is a valid move.
-        If the move is valid, updateCell
-
-        @param index - The index parameter represents the position of the cell in the board array
-        that the player clicked on
-    */
-    const playTurn = (index) => {
-        if (!gameOver) {
-            const validMove = Gameboard.updateCell(index, currentPlayer.markerIcon); // Gameboard.updateCell will update the cell content, then return boolean whether the move was valid or not.
-            console.log(`playTurn => validMove: ${validMove}`)
-            if (validMove) { // If the move is valid
-                // TODO: check for win or tie conditions
-                UI.renderGameboard(); // Re-renders the gameboard to display the the new moves
-                togglePlayer(); // Toggles the current player
-            }
-        }
-    };
-
-
-    /*
-    * The startGame arrow function initializes the game by setting the current player, rendering the gameboard,
-    * and attaching cell listeners.
-    */
-    const startGame = () => {
-        currentPlayer = player1;
-        currentPlayerElement.innerText = currentPlayer.name;
-        gameOver = false;
-        UI.renderGameboard();
-        UI.attachCellListeners();
-        // init game
-    };
-
-    return {startGame, playTurn};
-})();
-
+// UI Module
 const UI = (() => {
     /**
      * The `renderGameboard` function updates the display of the game board by setting the content of each
@@ -122,7 +70,83 @@ const UI = (() => {
         });
       };
 
-    return { renderGameboard, attachCellListeners };
+    const renderPregameMenu = () => {
+        const emojiList = document.getElementById('emoji-list');
+        const emojis = ['😀', '🥰', '🚀', '🎉', '❤️', '🌟', '😊', '🐱', '🍕', '🌈'];
+        // TODO: Add more options -> https://getemoji.com/
+        
+        emojis.forEach(emoji => {
+            const emojiSpan = document.createElement('span');
+            emojiSpan.classList.add('emoji');
+            emojiSpan.innerText = emoji;
+            emojiList.appendChild(emojiSpan);
+        })
+
+    }
+
+    return { renderGameboard, attachCellListeners, renderPregameMenu };
 })();
 
-Game.startGame();
+// Game Module
+const Game = (() => {
+    let currentPlayer;
+    let currentPlayerElement = document.getElementById('current-player');
+    let gameStatsElement = document.getElementById('game-stats');
+    let gameOver = false;
+
+    // Declaring players and setting default values
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+
+    // Arrow function to toggle the currentPlayer, then adjusts the text in the currentPlayerElement
+    const togglePlayer = () => {
+        currentPlayer = (currentPlayer === player1) ? player2 : player1;
+        currentPlayerElement.innerText = currentPlayer.name;
+    };
+
+    /*
+        Arrow function to play a turn of the game.
+        It calls the updateCell method from the Gameboard module to check if it is a valid move.
+        If the move is valid, updateCell
+
+        @param index - The index parameter represents the position of the cell in the board array
+        that the player clicked on
+    */
+    const playTurn = (index) => {
+        if (!gameOver) {
+            const validMove = Gameboard.updateCell(index, currentPlayer.markerIcon); // Gameboard.updateCell will update the cell content, then return boolean whether the move was valid or not.
+            console.log(`playTurn => validMove: ${validMove}`)
+            if (validMove) { // If the move is valid
+                // TODO: check for win or tie conditions
+                UI.renderGameboard(); // Re-renders the gameboard to display the the new moves
+                togglePlayer(); // Toggles the current player
+            }
+        }
+    };
+
+    /*
+    * The startGame arrow function initializes the game by setting the current player, rendering the gameboard,
+    * and attaching cell listeners.
+    */
+    const startGame = () => {
+        currentPlayer = player1;
+        currentPlayerElement.innerText = currentPlayer.name;
+        gameStatsElement.style.visibility = 'visible'; // reveals the game-stats div
+        gameOver = false;
+        UI.renderGameboard();
+        UI.attachCellListeners();
+    };
+
+    let startGameButton = document.getElementById('start-game-btn');
+    startGameButton.addEventListener('click', () => {
+        startGame();
+    });
+
+    UI.renderPregameMenu();
+
+    return {startGame, playTurn};
+})();
+
+
+
+// Game.startGame();
